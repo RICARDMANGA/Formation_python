3d594d94fbf98deaa9de9786065f66f3b54ebd6f
RICARDMANGA <mangaricardsilongue@gmail.com>
2025-05-25 17:53:10 +0000

    Take Conditional Input from user in Python

--- Input_and_Output_in_Python.py
+++ Input_and_Output_in_Python.py
@@ -1,10 +1,16 @@
-# taking two inputs at a time
-x, y = input("Enter two values: ").split()
-print("Number of boys: ", x)
-print("Number of girls: ", y)
- 
-# taking three inputs at a time
-x, y, z = input("Enter three values: ").split()
-print("Total number of students: ", x)
-print("Number of boys is : ", y)
-print("Number of girls is : ", z)
+
+# Prompting the user for input
+age_input = input("Enter your age: ")
+
+# Converting the input to an integer
+age = int(age_input)
+
+# Checking conditions based on user input
+if age < 0:
+    print("Please enter a valid age.")
+elif age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 65:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
