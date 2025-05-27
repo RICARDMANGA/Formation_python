63c2daf3caf6bc5afc659dd18d5b195529a1f748
RICARDMANGA <mangaricardsilongue@gmail.com>
2025-05-27 02:10:14 +0000

    Create a Set in Python Create a Set in Python

--- Python_Data_Types.py
+++ Python_Data_Types.py
@@ -1,9 +1,10 @@
-set1 = set(["Geeks", "For", "Geeks"])
-print(set1)
+#Create a Set in Python 
 
-# loop through set
-for i in set1:
-    print(i, end=" ")
-    
-# check if item exist in set    
-print("Geeks" in set1)
+# initializing empty set
+s1 = set()
+
+s1 = set("GeeksForGeeks")
+print("Set with the use of String: ", s1)
+
+s2 = set(["Geeks", "For", "Geeks"])
+print("Set with the use of List: ", s2)
