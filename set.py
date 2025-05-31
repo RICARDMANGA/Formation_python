ef8447f84cd92b04075003f3d4612fe857f14453
RICARDMANGA <mangaricardsilongue@gmail.com>
2025-05-31 15:38:00 +0000

    Using remove() Method or discard() Method

--- set.py
+++ set.py
@@ -1,10 +1,29 @@
-# Creating a set
-set1 = {1, 2, 3}
 
-# Add one item
-set1.add(4)
 
-# Add multiple items
-set1.update([5, 6])
 
+
+
+
+
+
+
+
+
+# Using Remove Method
+set1 = {1, 2, 3, 4, 5}
+set1.remove(3)
+print(set1)  
+
+# Attempting to remove an element that does not exist
+try:
+    set1.remove(10)
+except KeyError as e:
+    print("Error:", e)  
+
+# Using discard() Method
+set1.discard(4)
+print(set1)  
+
+# Attempting to discard an element that does not exist
+set1.discard(10)  # No error raised
 print(set1)
