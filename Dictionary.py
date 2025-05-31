3bc8cc6fd6e7c0566bf642eb45af806469dea2fd
RICARDMANGA <mangaricardsilongue@gmail.com>
2025-05-31 14:41:18 +0000

    Removing Dictionary Items

--- Dictionary.py
+++ Dictionary.py
@@ -1,9 +1,18 @@
-d = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+d = {1: 'Geeks', 2: 'For', 3: 'Geeks', 'age':22}
 
-# Adding a new key-value pair
-d["age"] = 22
+# Using del to remove an item
+del d["age"]
+print(d)
 
-# Updating an existing value
-d[1] = "Python dict"
+# Using pop() to remove an item and return the value
+val = d.pop(1)
+print(val)
 
+# Using popitem to removes and returns
+# the last key-value pair.
+key, val = d.popitem()
+print(f"Key: {key}, Value: {val}")
+
+# Clear all items from the dictionary
+d.clear()
 print(d)
