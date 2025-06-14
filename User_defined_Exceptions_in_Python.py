5dd3812c37847ba14908a54979e8e52e9fcd05ad
RICARDMANGA <mangaricardsilongue@gmail.com>
2025-06-14 20:24:08 +0000

    Customizing Exception Classes

--- User_defined_Exceptions_in_Python.py
+++ User_defined_Exceptions_in_Python.py
@@ -20,3 +20,35 @@ try:
     set_age(150)  # This will raise the custom exception
 except InvalidAgeError as e:
     print(e)
+
+
+
+    # Step 1: Subclass the Exception class
+
+class InvalidAgeError(Exception):
+    def __init__(self, age, msg="Age must be between 0 and 120", error_code=1001):
+        # Custom attributes
+        self.age = age
+        self.msg = msg
+        self.error_code = error_code
+        super().__init__(self.msg)  # Call the base class constructor
+
+    # Step 2: Customize the string representation of the exception
+    
+    def __str__(self):
+        return f"[Error Code {self.error_code}] {self.age} -> {self.msg}"
+
+# Step 3: Raising the custom exception
+
+def set_age(age):
+    if age < 0 or age > 120:
+        raise InvalidAgeError(age)
+    else:
+        print(f"Age set to: {age}")
+
+# Step 4: Handling the custom exception with additional information
+
+try:
+    set_age(150)  # This will raise the custom exception
+except InvalidAgeError as e:
+    print(e)
